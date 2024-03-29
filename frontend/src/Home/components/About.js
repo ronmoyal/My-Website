fdb5c6098fd0aaed4a2a38e5fe812401048d896f
Ron Moyal <95085650+ronmoyal@users.noreply.github.com>
2024-03-29 04:08:18 +0300

    Update About.js

--- frontend/src/Home/components/About.js
+++ frontend/src/Home/components/About.js
@@ -7,11 +7,12 @@ const About = () => {
     return (
         <div className="banner">
             <h1 className="my-name">Hi! I'm <span className="highlight">Ron Moyal</span></h1>
-            <h2 className="web-development-title">WEB DEVELOPMENT</h2>
+            <h2 className="web-development-title">SOFTWARE ENGINEER</h2>
                 
-                <div className="aboutme">
-                    <p>Welcome! I'm a Fourth-year Software Engineering student at Sami Shamoon College of Engineering with a strong foundation in programming and problem-solving. I've had the privilege of working on various projects, honing my skills in languages and technologies such as Java, MERN Stack, C#, and HTML.</p>
-                    <p>These experiences have not only strengthened my technical skills but also developed my teamwork capabilities and adaptability to ever-evolving challenges.</p>
+            <div className="aboutme">
+                    <p>Welcome! I'm a Fourth-year Software Engineering student at Sami Shamoon College of Engineering with a strong foundation in programming and problem-solving. I've had the privilege of working on various projects, honing my skills in languages and technologies such as Java, MERN Stack, C#, and React Native.</p>
+                    <p>Additionally, I've furthered my full-stack development capabilities by completing a comprehensive MERN Stack course on Udemy. This experience has not only strengthened my technical skills but also expanded my proficiency in developing modern web applications.</p>
+                    <p>These experiences have developed my teamwork capabilities and adaptability to ever-evolving challenges, enriching my journey in the software engineering realm.</p>
                     <p>This website showcases my projects, skills, and experiences, providing an insight into my journey in the software engineering realm. Thank you for your time and for visiting my site!</p>
                 </div>
 
